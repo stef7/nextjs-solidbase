55fdaa43083d765dff4a3df732fd8b81eead54cc
Stefano Perfili <1480673+stef7@users.noreply.github.com>
2022-09-15 01:49:52 +1000

    node only req major version

--- scripts/check-node-version.js
+++ scripts/check-node-version.js
@@ -2,9 +2,9 @@
 const { readFileSync } = require("node:fs");
 const path = require("node:path");
 
-const requiredVersion = readFileSync(path.resolve(".nvmrc"), { encoding: "utf8" }).trim();
+const requiredVersion = readFileSync(path.resolve(".nvmrc"), { encoding: "utf8" }).match(/^\D*\d+/)[0];
 
-const currentVersion = process.versions.node;
+const currentVersion = process.versions.node.match(/^\D*\d+/)[0];
 
 if (currentVersion !== requiredVersion)
-  throw new Error(`Node.js version ${requiredVersion} required. Currently using version ${currentVersion}.`);
+  throw new Error(`Node.js major version ${requiredVersion} required, currently using ${currentVersion}.`);
