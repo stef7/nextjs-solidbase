631bbdd8963f2953fd2420ef0c49183edc67f3a9
Stefano Perfili <1480673+stef7@users.noreply.github.com>
2022-09-15 02:09:35 +1000

    test tweaks

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -13,4 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+process.env.NODE_ENV = "test";
+
 import "./commands.ts";
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 // https://nextjs.org/docs/testing#jest-and-react-testing-library
 const nextJest = require("next/jest");
 
+process.env.NODE_ENV = "test";
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
