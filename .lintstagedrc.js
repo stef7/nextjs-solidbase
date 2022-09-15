9698a83f9e4f9e843bae7cdfdcfb199ae8c8d279
Stefano Perfili <1480673+stef7@users.noreply.github.com>
2022-09-15 12:29:08 +1000

    next lint includes prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,6 @@
 const path = require("path");
 
 module.exports = {
-  "*.(j|t)s(x|)": [
-    (filenames) => `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`,
-    "prettier --write",
-  ],
+  "*.(j|t)s(x|)": (filenames) =>
+    `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`,
 };
