2560882bfb23e59541657b5556536c1a16f37b58
Stefano Perfili <1480673+stef7@users.noreply.github.com>
2022-09-16 19:17:53 +1000

    stylelint staged fix?

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,5 +5,5 @@ const path = require("path");
 module.exports = {
   "*.(j|t)s(x|)": (filenames) =>
     `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`,
-  "*.css": "prettier --fix",
+  "*.css": `stylelint --fix`,
 };
